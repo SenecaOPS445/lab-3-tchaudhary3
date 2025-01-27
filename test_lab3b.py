7505df2ea3f0933bcd56ba7fb17c2dfaf7af10e5
Tania Chaudhary <tchaudhary3@myseneca.ca>
2025-01-27 13:42:28 -0500

    Add untracked files

---
+++ test_lab3b.py
@@ -0,0 +1,13 @@
+import lab3b
+
+# Test sum_numbers function
+print(lab3b.sum_numbers(10, 5))  # Should return 15
+print(lab3b.sum_numbers(25, 25))  # Should return 50
+
+# Test subtract_numbers function
+print(lab3b.subtract_numbers(10, 5))  # Should return 5
+print(lab3b.subtract_numbers(5, 10))  # Should return -5
+
+# Test multiply_numbers function
+print(lab3b.multiply_numbers(10, 5))  # Should return 50
+print(lab3b.multiply_numbers(10, 2))  # Should return 20
